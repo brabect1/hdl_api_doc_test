c4b687b2fdf165a000aee9b2f1226a681bdba3cc
Tomas Brabec <xtbr123@gmail.com>
2018-11-01 21:02:52 +0100

    Started towards netural docs setup.

--- natural_docs/src/apb_agent/apb_agent.svh
+++ natural_docs/src/apb_agent/apb_agent.svh
@@ -16,10 +16,23 @@
 //   the License for the specific language governing
 //   permissions and limitations under the License.
 //------------------------------------------------------------
+
+//------------------------------------------------------------------------------
 //
-// Class Description:
+// CLASS: apb_agent
 //
+// Implements the UVC Agent component for Amba Peripheral Bus (APB).
 //
+// The implementation comes from the Verification Cookbook at the Verification
+// Academy site (https://verificationacademy.com/). It follows the conventional
+// UVC agent structure.
+//
+// The agent consists of the following parts: Driver, monitor, sequencer and
+// a coverage collection class. The agent is configured through a specific
+// configuration object. The agent exports its monitor's analysis port.
+//
+//------------------------------------------------------------------------------
+
 class apb_agent extends uvm_component;
 
 // UVM Factory Registration Macro
@@ -29,23 +42,72 @@ class apb_agent extends uvm_component;
 //------------------------------------------
 // Data Members
 //------------------------------------------
+
+// Variable: m_cfg
+//
+// Agent configuration. It will be obtained from the UVM Configuration DB during
+// the build phase, under the keyword `apb_agent_config`.
+
 apb_agent_config m_cfg;
 //------------------------------------------
 // Component Members
 //------------------------------------------
+
+// Variable: ap
+// Analysis port exported from the agent's monitor component.
+
 uvm_analysis_port #(apb_seq_item) ap;
+
+// Variable: m_monitor
+// An instance of the APB monitor. This component is always present (i.e. even
+// in the passive mode).
 apb_monitor   m_monitor;
+
+// Variable: m_sequencer
+// An instance of the APB sequencer. It is present only in the active mode.
 apb_sequencer m_sequencer;
+
+// Variable: m_driver
+// An instance of the APB driver. It is present only in the active mode.
 apb_driver    m_driver;
+
+// Variable: m_fcov_monitor
+// An instance collecting functional coverage. It is present only if the agent
+// was configured with enabled coverage collection.
 apb_coverage_monitor m_fcov_monitor;
 //------------------------------------------
 // Methods
 //------------------------------------------
 
 // Standard UVM Methods:
-extern function new(string name = "apb_agent", uvm_component parent = null);
-extern function void build_phase(uvm_phase phase);
-extern function void connect_phase(uvm_phase phase);
+
+
+// Function: new
+//
+// Implements the default UVM component constructor.
+
+extern function new(
+    //! Name of the agent component. This name will be associated with the agent
+    //! instance and may be used for looking up the instance in the UVM Config DB.
+    string name = "apb_agent",
+    //! Reference to a parent component, which the agent instance will be a part of.
+    uvm_component parent = null
+);
+
+// Function: build_phase
+// Builds individual components based on configured agent's settings.
+
+extern function void build_phase(
+    //! Reference to the build phase instance.
+    uvm_phase phase
+);
+
+// Function: connect_phase
+// Connects all the components that have been created suring the build phase.
+extern function void connect_phase(
+    //! Reference to the connect phase instance.
+    uvm_phase phase
+);
 
 endclass: apb_agent
 
